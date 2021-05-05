e35111f68df2a6289049ab8f2e3741d971ddf9f0
HuyDTran121 <huy.tran.tx@gmail.com>
2021-05-05 17:33:54 -0500

    Glasses now rotate

--- face_detect.py
+++ face_detect.py
@@ -12,7 +12,7 @@ image = image_processor.loadImage("./data/face_1.jpg")
 
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-bounding_boxes = face_cascade.detectMultiScale(grayscale_image, scaleFactor = 1.2, minNeighbors = 5);
+bounding_boxes = face_cascade.detectMultiScale(grayscale_image, scaleFactor = 1.2, minNeighbors = 5)
 print(type(face_cascade))
 print(type(bounding_boxes))
 print(bounding_boxes)
